f98b7fb6426cd5cdafdff197056cfd341420e31a
EmielZuurbier <emielzuurbier@outlook.com>
2021-09-07 21:02:16 +0200

    Fix indentation

--- app/Assets/Scripts.php
+++ app/Assets/Scripts.php
@@ -4,189 +4,189 @@ namespace App\Assets;
 
 class Scripts
 {
-    /**
-     * If the scripts should be placed in the wp_foot position.
-     * @var boolean
-     */
-    private $in_footer = true;
+  /**
+   * If the scripts should be placed in the wp_foot position.
+   * @var boolean
+   */
+  private $in_footer = true;
 
-    /**
-     * Add after_setup_theme hook.
-     */
-    public function __construct()
-    {
-        add_action('after_setup_theme', [$this, 'init'], 10);
+  /**
+   * Add after_setup_theme hook.
+   */
+  public function __construct()
+  {
+    add_action('after_setup_theme', [$this, 'init'], 10);
+  }
+
+  /**
+   * Add filters and hooks after theme setup.
+   * @link https://developer.wordpress.org/reference/hooks/after_setup_theme/
+   */
+  public function init()
+  {
+    add_filter('script_loader_tag', [$this, 'defer_scripts'], 10, 2);
+    add_action('wp_head', [$this, 'javascript_active'], 1);
+    add_action('wp_head', [$this, 'preload_scripts'], 1);
+    add_action('wp_enqueue_scripts', [$this, 'dequeue'], 10);
+    add_action('wp_enqueue_scripts', [$this, 'enqueue'], 10);
+  }
+
+  /**
+   * Returns nested arrays of data that will be used when enqueueing the scripts.
+   * Allows for inline scripts to be created.
+   * @return array[]
+   */
+  public function scripts()
+  {
+    return [
+      [
+        'handle'        => 'app',
+        'src'           => \App\asset_path('scripts/app.js'),
+        'dependencies'  => [],
+        'version'       => false,
+        'inline'        => [
+          'js_key'        => 'appData',
+          'data'          => [
+            'endpoints'     => [
+              'page'          => esc_url(rest_url('5euros/v1/page')),
+              'data'          => esc_url(rest_url('5euros/v1/data')),
+              'search'        => esc_url(rest_url('5euros/v1/search')),
+            ],
+            'worker'        => \App\asset_path('scripts/worker.js'),
+            'serviceWorker' => \App\asset_path('scripts/serviceworker.js'),
+            'worklets'      => [
+              'bitCrusher'   => \App\asset_path('scripts/bit-crusher-worklet.js'),
+            ],
+            'nonce'         => wp_create_nonce('wp_rest')
+          ],
+          'position'      => 'before'
+        ],
+        'preload'       => true,
+        'defer'         => true
+      ]
+    ];
+  }
+
+  /**
+   * Dequeues scripts that shouldn't be included.
+   */
+  public function dequeue()
+  {
+    $scripts = [
+      'wp-embed',
+      'jquery'
+    ];
+
+    foreach ($scripts as $script) {
+      wp_deregister_script($script);
     }
+  }
 
-    /**
-     * Add filters and hooks after theme setup.
-     * @link https://developer.wordpress.org/reference/hooks/after_setup_theme/
-     */
-    public function init()
-    {
-        add_filter('script_loader_tag', [$this, 'defer_scripts'], 10, 2);
-        add_action('wp_head', [$this, 'javascript_active'], 1);
-        add_action('wp_head', [$this, 'preload_scripts'], 1);
-        add_action('wp_enqueue_scripts', [$this, 'dequeue'], 10);
-        add_action('wp_enqueue_scripts', [$this, 'enqueue'], 10);
+  /**
+   * Enqueues scripts based on the data in the scripts array.
+   * Whenever an inline key is present it will also include an inline script.
+   */
+  public function enqueue()
+  {
+    $scripts = $this->scripts();
+
+    foreach ($scripts as $script) {
+      [
+        'handle'       => $handle,
+        'src'          => $src,
+        'dependencies' => $dependencies,
+        'version'      => $version
+      ] = $script;
+
+      wp_register_script(
+        $handle,
+        $src,
+        $dependencies,
+        $version,
+        $this->in_footer
+      );
+
+      if (isset($script['inline'])) {
+        $inline = $script['inline'];
+
+        [
+          'js_key'    => $js_key,
+          'data'      => $data,
+          'position'  => $position
+        ] = $inline;
+
+        $json_data = json_encode($data);
+
+        wp_add_inline_script(
+          $handle,
+          "window.__{$js_key}__ = $json_data",
+          $position
+        );
+      }
+
+      wp_enqueue_script($handle);
     }
+  }
 
-    /**
-     * Returns nested arrays of data that will be used when enqueueing the scripts.
-     * Allows for inline scripts to be created.
-     * @return array[]
-     */
-    public function scripts()
-    {
-        return [
-            [
-                'handle'        => 'app',
-                'src'           => \App\asset_path('scripts/app.js'),
-                'dependencies'  => [],
-                'version'       => false,
-                'inline'        => [
-                    'js_key'        => 'appData',
-                    'data'          => [
-                        'endpoints'     => [
-                            'page'          => esc_url(rest_url('5euros/v1/page')),
-                            'data'          => esc_url(rest_url('5euros/v1/data')),
-                            'search'        => esc_url(rest_url('5euros/v1/search')),
-                        ],
-                        'worker'        => \App\asset_path('scripts/worker.js'),
-                        'serviceWorker' => \App\asset_path('scripts/serviceworker.js'),
-                        'worklets'      => [
-                            'bitCrusher'   => \App\asset_path('scripts/bit-crusher-worklet.js'),
-                        ],
-                        'nonce'         => wp_create_nonce('wp_rest')
-                    ],
-                    'position'      => 'before'
-                ],
-                'preload'       => true,
-                'defer'         => true
-            ]
-        ];
-    }
+  /**
+   * Outputs a script in head that removes the no-js class from the root tag to indicate that JS is active.
+   * @link https://developer.wordpress.org/reference/hooks/wp_head/
+   */
+  public function javascript_active()
+  {
+    echo "<script>document.documentElement.className = document.documentElement.className.replace('no-js', '');</script>";
+  }
 
-    /**
-     * Dequeues scripts that shouldn't be included.
-     */
-    public function dequeue()
-    {
-        $scripts = [
-            'wp-embed',
-            'jquery'
-        ];
+  /**
+   * Output a preload tag in the head to indicate that we need this script soon.
+   * @link https://developer.wordpress.org/reference/hooks/wp_head/
+   */
+  public function preload_scripts()
+  {
+    $scripts = $this->scripts();
 
-        foreach ($scripts as $script) {
-            wp_deregister_script($script);
+    if (!is_admin()) {
+      foreach ($scripts as $script) {
+        if (isset($script['preload']) && $script['preload'] === true) {
+          echo '<link rel="preload" href="' . $script['src'] . '" as="script">';
         }
+      }
+    }
+  }
+
+  /**
+   * Add attributes to the script tag.
+   * Can be used to add a 'async' or 'defer' attribute to a script tag.
+   * @link https://developer.wordpress.org/reference/hooks/script_loader_tag/
+   * @param string $tag
+   * @param string $handle
+   * @return string
+   */
+  public function defer_scripts($tag, $handle)
+  {
+    $handles = $this->get_handles_with('defer', true);
+
+    if (!is_admin() && in_array($handle, $handles)) {
+      return str_replace(' src', ' defer="defer" src', $tag);
     }
 
-    /**
-     * Enqueues scripts based on the data in the scripts array.
-     * Whenever an inline key is present it will also include an inline script.
-     */
-    public function enqueue()
-    {
-        $scripts = $this->scripts();
+    return $tag;
+  }
 
-        foreach ($scripts as $script) {
-            [
-                'handle'       => $handle,
-                'src'          => $src,
-                'dependencies' => $dependencies,
-                'version'      => $version
-            ] = $script;
+  /**
+   * Returns an array of handles based on the scripts.
+   * @return string[]
+   */
+  private function get_handles_with($property, $value)
+  {
+    $scripts = $this->scripts();
 
-            wp_register_script(
-                $handle,
-                $src,
-                $dependencies,
-                $version,
-                $this->in_footer
-            );
+    $filtered_scripts = array_filter($scripts, function ($script) use ($property, $value) {
+      return isset($script[$property]) && $script[$property] === $value;
+    });
 
-            if (isset($script['inline'])) {
-                $inline = $script['inline'];
-
-                [
-                    'js_key'    => $js_key,
-                    'data'      => $data,
-                    'position'  => $position
-                ] = $inline;
-
-                $json_data = json_encode($data);
-
-                wp_add_inline_script(
-                    $handle,
-                    "window.__{$js_key}__ = $json_data",
-                    $position
-                );
-            }
-
-            wp_enqueue_script($handle);
-        }
-    }
-
-    /**
-     * Outputs a script in head that removes the no-js class from the root tag to indicate that JS is active.
-     * @link https://developer.wordpress.org/reference/hooks/wp_head/
-     */
-    public function javascript_active()
-    {
-        echo "<script>document.documentElement.className = document.documentElement.className.replace('no-js', '');</script>";
-    }
-
-    /**
-     * Output a preload tag in the head to indicate that we need this script soon.
-     * @link https://developer.wordpress.org/reference/hooks/wp_head/
-     */
-    public function preload_scripts()
-    {
-        $scripts = $this->scripts();
-
-        if (!is_admin()) {
-            foreach ($scripts as $script) {
-                if (isset($script['preload']) && $script['preload'] === true) {
-                    echo '<link rel="preload" href="' . $script['src'] . '" as="script">';
-                }
-            }
-        }
-    }
-
-    /**
-     * Add attributes to the script tag.
-     * Can be used to add a 'async' or 'defer' attribute to a script tag.
-     * @link https://developer.wordpress.org/reference/hooks/script_loader_tag/
-     * @param string $tag
-     * @param string $handle
-     * @return string
-     */
-    public function defer_scripts($tag, $handle)
-    {
-        $handles = $this->get_handles_with('defer', true);
-
-        if (!is_admin() && in_array($handle, $handles)) {
-            return str_replace(' src', ' defer="defer" src', $tag);
-        }
-
-        return $tag;
-    }
-
-    /**
-     * Returns an array of handles based on the scripts.
-     * @return string[]
-     */
-    private function get_handles_with($property, $value)
-    {
-        $scripts = $this->scripts();
-
-        $filtered_scripts = array_filter($scripts, function ($script) use ($property, $value) {
-            return isset($script[$property]) && $script[$property] === $value;
-        });
-
-        return array_map(function ($script) {
-            return $script['handle'];
-        }, $filtered_scripts);
-    }
+    return array_map(function ($script) {
+      return $script['handle'];
+    }, $filtered_scripts);
+  }
 }
--- app/Assets/Styles.php
+++ app/Assets/Styles.php
@@ -4,139 +4,139 @@ namespace App\Assets;
 
 class Styles
 {
-    /**
-     * Add after_setup_theme hook.
-     */
-    public function __construct()
-    {
-        add_action('after_setup_theme', [$this, 'init'], 10);
+  /**
+   * Add after_setup_theme hook.
+   */
+  public function __construct()
+  {
+    add_action('after_setup_theme', [$this, 'init'], 10);
+  }
+
+  /**
+   * Add filters and actions.
+   * @link https://developer.wordpress.org/reference/hooks/after_setup_theme/
+   */
+  public function init()
+  {
+    add_filter('style_loader_tag', [$this, 'lazy_styles'], 10, 4);
+    add_action('wp_head', [$this, 'preload_styles'], 1);
+    add_action('wp_enqueue_scripts', [$this, 'dequeue'], 10);
+    add_action('wp_enqueue_scripts', [$this, 'enqueue'], 10);
+  }
+
+  /**
+   * Returns nested arrays of data that will be used when enqueueing the styles.
+   * @return array[]
+   */
+  public function styles()
+  {
+    return [
+      [
+        'handle'        => 'app',
+        'src'           => \App\asset_path('styles/app.css'),
+        'dependencies'  => [],
+        'version'       => false,
+        'media'         => 'all',
+        'preload'       => true,
+        'lazy'          => false
+      ]
+    ];
+  }
+
+  /**
+   * Dequeues styles that shouldn't be included.
+   */
+  public function dequeue()
+  {
+    $styles = [
+      'wp-block-library'
+    ];
+
+    foreach ($styles as $style) {
+      wp_deregister_style($style);
     }
+  }
 
-    /**
-     * Add filters and actions.
-     * @link https://developer.wordpress.org/reference/hooks/after_setup_theme/
-     */
-    public function init()
-    {
-        add_filter('style_loader_tag', [$this, 'lazy_styles'], 10, 4);
-        add_action('wp_head', [$this, 'preload_styles'], 1);
-        add_action('wp_enqueue_scripts', [$this, 'dequeue'], 10);
-        add_action('wp_enqueue_scripts', [$this, 'enqueue'], 10);
+  /**
+   * Enqueues styles based on the data in the styles array.
+   */
+  public function enqueue()
+  {
+    $styles = $this->styles();
+
+    foreach ($styles as $style) {
+      [
+        'handle'       => $handle,
+        'src'          => $src,
+        'dependencies' => $dependencies,
+        'version'      => $version,
+        'media'        => $media
+      ] = $style;
+
+      wp_register_style(
+        $handle,
+        $src,
+        $dependencies,
+        $version,
+        $media
+      );
+
+      wp_enqueue_style($handle);
     }
+  }
 
-    /**
-     * Returns nested arrays of data that will be used when enqueueing the styles.
-     * @return array[]
-     */
-    public function styles()
-    {
-        return [
-            [
-                'handle'        => 'app',
-                'src'           => \App\asset_path('styles/app.css'),
-                'dependencies'  => [],
-                'version'       => false,
-                'media'         => 'all',
-                'preload'       => true,
-                'lazy'          => false
-            ]
-        ];
-    }
+  /**
+   * Adds a preload tag before the stylesheet tag.
+   * @return string
+   */
+  public function preload_styles()
+  {
+    $styles = $this->styles();
 
-    /**
-     * Dequeues styles that shouldn't be included.
-     */
-    public function dequeue()
-    {
-        $styles = [
-            'wp-block-library'
-        ];
-
-        foreach ($styles as $style) {
-            wp_deregister_style($style);
+    if (!is_admin()) {
+      foreach ($styles as $style) {
+        if (isset($style['preload']) && $style['preload'] === true) {
+          echo '<link rel="preload" href="' . $style['src'] . '" as="style">';
         }
+      }
+    }
+  }
+
+  /**
+   * Modifies the link html to incorporate a lazy-loading tactic.
+   * @param string $html
+   * @param string $handle
+   * @param string $href
+   * @param string $media
+   * @return string
+   */
+  public function lazy_styles($html, $handle, $href, $media)
+  {
+    $handles = $this->get_handles_with('lazy', true);
+
+    if (!is_admin() && in_array($handle, $handles)) {
+      return "<link id='$handle-css' href='$href' rel='stylesheet' media='print' onload='this.media=\"$media\"'>";
     }
 
-    /**
-     * Enqueues styles based on the data in the styles array.
-     */
-    public function enqueue()
-    {
-        $styles = $this->styles();
+    return $html;
+  }
 
-        foreach ($styles as $style) {
-            [
-                'handle'       => $handle,
-                'src'          => $src,
-                'dependencies' => $dependencies,
-                'version'      => $version,
-                'media'        => $media
-            ] = $style;
+  /**
+   * Returns an array of handles based on the styles.
+   * @param string $property
+   * @param string $value
+   * @return string[]
+   */
+  private function get_handles_with($property, $value)
+  {
+    $styles = $this->styles();
 
-            wp_register_style(
-                $handle,
-                $src,
-                $dependencies,
-                $version,
-                $media
-            );
+    $filtered_styles = array_filter($styles, function ($style) use ($property, $value) {
+      return isset($style[$property]) && $style[$property] === $value;
+    });
 
-            wp_enqueue_style($handle);
-        }
-    }
-
-    /**
-     * Adds a preload tag before the stylesheet tag.
-     * @return string
-     */
-    public function preload_styles()
-    {
-        $styles = $this->styles();
-
-        if (!is_admin()) {
-            foreach ($styles as $style) {
-                if (isset($style['preload']) && $style['preload'] === true) {
-                    echo '<link rel="preload" href="' . $style['src'] . '" as="style">';
-                }
-            }
-        }
-    }
-
-    /**
-     * Modifies the link html to incorporate a lazy-loading tactic.
-     * @param string $html
-     * @param string $handle
-     * @param string $href
-     * @param string $media
-     * @return string
-     */
-    public function lazy_styles($html, $handle, $href, $media)
-    {
-        $handles = $this->get_handles_with('lazy', true);
-
-        if (!is_admin() && in_array($handle, $handles)) {
-            return "<link id='$handle-css' href='$href' rel='stylesheet' media='print' onload='this.media=\"$media\"'>";
-        }
-
-        return $html;
-    }
-
-    /**
-     * Returns an array of handles based on the styles.
-     * @param string $property
-     * @param string $value
-     * @return string[]
-     */
-    private function get_handles_with($property, $value)
-    {
-        $styles = $this->styles();
-
-        $filtered_styles = array_filter($styles, function ($style) use ($property, $value) {
-            return isset($style[$property]) && $style[$property] === $value;
-        });
-
-        return array_map(function ($style) {
-            return $style['handle'];
-        }, $filtered_styles);
-    }
+    return array_map(function ($style) {
+      return $style['handle'];
+    }, $filtered_styles);
+  }
 }
